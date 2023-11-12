8bd58ae30a7a8b1d8a317f5755d95503f320fce0
John Erickson <john.t.erickson@gmail.com>
2023-11-12 03:45:02 -0800

    add byte aligned concat alongside broccoli

--- src/bin/test_broccoli.rs
+++ src/bin/test_broccoli.rs
@@ -3,30 +3,83 @@
 #![allow(dead_code)]
 extern crate core;
 extern crate brotli_decompressor;
+use std::io::Write;
+
 use brotli_decompressor::{CustomRead, CustomWrite};
 use super::brotli::enc::BrotliEncoderParams;
 use super::brotli::concat::{BroCatli, BroCatliResult};
 use super::integration_tests::UnlimitedBuffer;
-static RANDOM_THEN_UNICODE : &'static [u8] = include_bytes!("../../testdata/random_then_unicode");
-static ALICE: &'static[u8]  = include_bytes!("../../testdata/alice29.txt");
-static UKKONOOA: &'static[u8]  = include_bytes!("../../testdata/ukkonooa");
-static ASYOULIKE: &'static[u8] = include_bytes!("../../testdata/asyoulik.txt");
-static BACKWARD65536: &'static [u8] = include_bytes!("../../testdata/backward65536");
-static DICTWORD: &'static [u8] = include_bytes!("../../testdata/ends_with_truncated_dictionary");
-static RANDOM10K: &'static [u8] = include_bytes!("../../testdata/random_org_10k.bin");
-static RANDOMTHENUNICODE: &'static [u8] = include_bytes!("../../testdata/random_then_unicode");
-static QUICKFOX: &'static [u8] = include_bytes!("../../testdata/quickfox_repeated");
-static EMPTY: &'static [u8] = &[];
+const RANDOM_THEN_UNICODE : &'static [u8] = include_bytes!("../../testdata/random_then_unicode");
+const ALICE: &'static[u8]  = include_bytes!("../../testdata/alice29.txt");
+const UKKONOOA: &'static[u8]  = include_bytes!("../../testdata/ukkonooa");
+const ASYOULIKE: &'static[u8] = include_bytes!("../../testdata/asyoulik.txt");
+const BACKWARD65536: &'static [u8] = include_bytes!("../../testdata/backward65536");
+const DICTWORD: &'static [u8] = include_bytes!("../../testdata/ends_with_truncated_dictionary");
+const RANDOM10K: &'static [u8] = include_bytes!("../../testdata/random_org_10k.bin");
+const RANDOMTHENUNICODE: &'static [u8] = include_bytes!("../../testdata/random_then_unicode");
+const QUICKFOX: &'static [u8] = include_bytes!("../../testdata/quickfox_repeated");
+const EMPTY: &'static [u8] = &[];
 use super::Rebox;
+
+const BYTE_ALIGNED_START_BLOCK_WINDOW_22: &'static [u8] = &[0x6b,0x00];
+const BYTE_ALIGNED_END_BLOCK: &'static [u8] = &[0x3];
+
 fn concat(files:&mut [UnlimitedBuffer],
           brotli_files:&mut [UnlimitedBuffer],
+          byte_aligned_brotli_files: &mut [UnlimitedBuffer],
           window_override:Option<u8>,
           bs: usize) {
   let mut obuffer = vec![0u8; bs];
-  let mut ibuffer = vec![0u8; bs];
+  let mut brotli_ibuffer = vec![0u8; bs];
+  let mut bytealigned_ibuffer = Vec::with_capacity(bs + BYTE_ALIGNED_START_BLOCK_WINDOW_22.len() + BYTE_ALIGNED_END_BLOCK.len());
   let mut ooffset = 0usize;
   let mut ioffset;
   let mut uboutput = UnlimitedBuffer::new(&[]);
+
+  // concat byte-aligned if files are provided
+  {
+    bytealigned_ibuffer.write_all(BYTE_ALIGNED_START_BLOCK_WINDOW_22).unwrap();
+
+    for (file, brotli) in files.iter_mut().zip(byte_aligned_brotli_files.iter_mut()) {
+
+      // test decompressing the indiviudal block
+      {
+        let mut block_ibuffer = UnlimitedBuffer::new(&[]);
+        block_ibuffer.write_all(BYTE_ALIGNED_START_BLOCK_WINDOW_22).unwrap();
+        block_ibuffer.write_all(brotli.data()).unwrap();
+        block_ibuffer.write_all(BYTE_ALIGNED_END_BLOCK).unwrap();
+
+        let mut rt = UnlimitedBuffer::new(&[]);
+        match super::decompress(&mut block_ibuffer, &mut rt, 65536, Rebox::default()) {
+          Ok(_) => {},
+          Err(e) => panic!("Error {:?}", e),
+        }        
+        assert_eq!(&rt.data(), &file.data());
+      }
+
+      // byte concat the compressed bare block
+      bytealigned_ibuffer.write_all(&brotli.data()).unwrap();
+    }
+
+    bytealigned_ibuffer.write_all(BYTE_ALIGNED_END_BLOCK).unwrap();
+
+    let mut bytealigned_ibuffer = UnlimitedBuffer::new(&bytealigned_ibuffer);
+    let mut rt = UnlimitedBuffer::new(&[]);
+    match super::decompress(&mut bytealigned_ibuffer, &mut rt, 65536, Rebox::default()) {
+      Ok(_) => {},
+      Err(e) => panic!("Error {:?}", e),
+    }
+    let mut offset = 0;
+    for file in files.iter_mut() {
+      file.reset_read();
+      assert_eq!(&rt.data()[offset..offset+file.data().len()],
+                file.data());
+      offset += file.data().len();
+    }
+    assert_eq!(offset, rt.data().len());
+  }
+
+  // concat normal 
   {
     let mut output = super::IoWriterWrapper(&mut uboutput);
     let mut bro_cat_li = match window_override {
@@ -39,14 +92,14 @@ fn concat(files:&mut [UnlimitedBuffer],
           let mut input = super::IoReaderWrapper(brotli);
           loop {
             ioffset = 0;
-            match input.read(&mut ibuffer[..]) {
+            match input.read(&mut brotli_ibuffer[..]) {
               Err(e) => panic!("{}", e),
               Ok(cur_read) => {
                 if cur_read == 0 {
                   break;
                 }
                 loop {
-                  match bro_cat_li.stream(&ibuffer[..cur_read], &mut ioffset,
+                  match bro_cat_li.stream(&brotli_ibuffer[..cur_read], &mut ioffset,
                                           &mut obuffer[..], &mut ooffset) {
                     BroCatliResult::NeedsMoreOutput => {
                       match output.write(&obuffer[..ooffset]) {
@@ -99,6 +152,7 @@ fn concat(files:&mut [UnlimitedBuffer],
       }
     }
   }
+
   let mut rt = UnlimitedBuffer::new(&[]);
   match super::decompress(&mut uboutput, &mut rt, 65536, Rebox::default()) {
     Ok(_) => {},
@@ -106,6 +160,7 @@ fn concat(files:&mut [UnlimitedBuffer],
   }
   let mut offset = 0;
   for file in files {
+    file.reset_read();
     assert_eq!(&rt.data()[offset..offset+file.data().len()],
                file.data());
     offset += file.data().len();
@@ -115,6 +170,7 @@ fn concat(files:&mut [UnlimitedBuffer],
 
 fn concat_many_subsets(files:&mut [UnlimitedBuffer],
                        brotli_files:&mut [UnlimitedBuffer],
+                       bytealign_brotli_files:&mut [UnlimitedBuffer],
                        window_override:Option<u8>) {
   let test_plans:[(usize,usize);4] = [(brotli_files.len(), 4096 * 1024), (4, 1), (3, 3),  (2, 4096)];
   for plan_bs in test_plans.iter() {
@@ -122,7 +178,8 @@ fn concat_many_subsets(files:&mut [UnlimitedBuffer],
     for index in 0..(brotli_files.len() - core::cmp::min(plan_bs.0 - 1, files_len)) {
       let file_subset = &mut files[index..core::cmp::min(index+plan_bs.0, files_len)];
       let brotli_subset = &mut brotli_files[index..core::cmp::min(index+plan_bs.0, files_len)];
-      concat(file_subset, brotli_subset, window_override, plan_bs.1);
+      let bytealign_subset = &mut bytealign_brotli_files[index..core::cmp::min(index+plan_bs.0, files_len)];
+      concat(file_subset, brotli_subset, bytealign_subset, window_override, plan_bs.1);
     }
   }
 }
@@ -157,12 +214,22 @@ fn test_appendonly_twice_fails() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = true;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files[..], &mut ufiles[..], &mut byte_aligned_ufiles, None, 2);
 }
 
 #[test]
@@ -175,16 +242,27 @@ fn test_append_then_empty_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files[..], &mut ufiles[..], &mut byte_aligned_ufiles[..], None, 2);
 }
 
 #[test]
@@ -197,16 +275,27 @@ fn test_append_then_cat_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None, 2);
 }
 
 #[test]
@@ -219,16 +308,27 @@ fn test_one_byte_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None, 2);
 }
 
 #[test]
@@ -241,16 +341,27 @@ fn test_one_byte_before_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None, 2);
 }
 
 #[test]
@@ -263,16 +374,27 @@ fn test_two_byte_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None, 2);
 }
 
 #[test]
@@ -285,16 +407,27 @@ fn test_two_byte_before_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None, 2);
 }
 
 
@@ -309,16 +442,28 @@ fn test_empty_then_cat_works() {
     UnlimitedBuffer::new(&[]),
     UnlimitedBuffer::new(&[]),
   ];
+  let mut byte_aligned_ufiles = [
+    UnlimitedBuffer::new(&[]),
+    UnlimitedBuffer::new(&[]),
+  ];
   let mut first = true;
-  for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+  let buffers = files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut());
+  for ((src, dst), align_dst) in buffers {
     let mut params0 = BrotliEncoderParams::default();
     params0.appendable = first;
     params0.catable = !first;
     params0.use_dictionary = first;
     super::compress(src, dst, 4096, &params0, &[], 1).unwrap();
+
+    src.reset_read();
+    let mut params0 = BrotliEncoderParams::default();
+    byte_align_params(&mut params0);
+
+    super::compress(src, align_dst, 4096, &params0, &[], 1).unwrap();
+
     first = false;
   }
-  concat(&mut files[..], &mut ufiles[..], None, 2);
+  concat(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None, 2);
 }
 
 #[test]
@@ -381,8 +526,20 @@ fn test_concat() {
             UnlimitedBuffer::new(&[]),
             UnlimitedBuffer::new(&[]),
         ];
+        let mut byte_aligned_ufiles = [
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+            UnlimitedBuffer::new(&[]),
+        ];
         let mut first = true;
-        for (src, dst) in files.iter_mut().zip(ufiles.iter_mut()) {
+        for ((src, dst), align_dst) in files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut()) {
+            let mut block_align_option = option.clone();
+
             if first {
                 option.appendable = true;
             } else {
@@ -391,10 +548,15 @@ fn test_concat() {
                 option.use_dictionary = false;
             }
             super::compress(src, dst, 4096, option, &[], 1).unwrap();
+
+            src.reset_read();
+            byte_align_params(&mut block_align_option);
+            super::compress(src, align_dst, 4096, &block_align_option, &[], 1).unwrap();
+
             src.reset_read();
             first = false;
         }
-        concat_many_subsets(&mut files[..], &mut ufiles[..], None);
+        concat_many_subsets(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None);
         return;
     }
     let mut ufiles = [
@@ -407,18 +569,41 @@ fn test_concat() {
       UnlimitedBuffer::new(&[]),
       UnlimitedBuffer::new(&[]),
     ];
+    let mut byte_aligned_ufiles = [
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+      UnlimitedBuffer::new(&[]),
+    ];
     let options_len = options.len();
-    for (index, (src, dst)) in files.iter_mut().zip(ufiles.iter_mut()).enumerate() {
-      options[core::cmp::min(index, options_len - 1)].catable = true;
-      options[core::cmp::min(index, options_len - 1)].use_dictionary = false;
-      options[core::cmp::min(index, options_len - 1)].appendable = false;
-      options[core::cmp::min(index, options_len - 1)].quality = core::cmp::max(
-        2, options[core::cmp::min(index, options_len - 1)].quality);
+    for (index, ((src, dst), align_dst)) in files.iter_mut().zip(ufiles.iter_mut()).zip(byte_aligned_ufiles.iter_mut()).enumerate() {
+      let option = &mut options[core::cmp::min(index, options_len - 1)];
+      option.catable = true;
+      option.use_dictionary = false;
+      option.appendable = false;
+      option.quality = core::cmp::max(2, option.quality);
       // ^^^ there's an artificial limitation of using 18 as the minimum window size for quality 0,1
       // since this test depends on different window sizes for each stream, exclude q={0,1}  
-      super::compress(src, dst, 4096, &options[core::cmp::min(index, options_len - 1)], &[], 1).unwrap();
+      super::compress(src, dst, 4096, option, &[], 1).unwrap();
+
+      src.reset_read();
+      let mut block_align_option = option.clone();
+      byte_align_params(&mut block_align_option);
+      super::compress(src, align_dst, 4096, &block_align_option, &[], 1).unwrap();
+
       src.reset_read();
     }
-    concat_many_subsets(&mut files[..], &mut ufiles[..], None);
-    concat_many_subsets(&mut files[..], &mut ufiles[..], Some(28)); // FIXME: make this 28
+    concat_many_subsets(&mut files, &mut ufiles, &mut byte_aligned_ufiles, None);
+    concat_many_subsets(&mut files, &mut ufiles, &mut byte_aligned_ufiles, Some(28)); // FIXME: make this 28
+}
+
+fn byte_align_params(option: &mut BrotliEncoderParams) {
+    option.byte_align = true;
+    option.bare_stream = true;
+    option.use_dictionary = false;
+    option.catable = true;
 }
